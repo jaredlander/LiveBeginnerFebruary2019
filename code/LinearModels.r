f02383132eb9319a9ad30104f2db01a16d5df40a
Jared Lander <info@jaredlander.com>
2019-02-05 15:15:28 -0500

    Showed multiple regression

--- code/LinearModels.r
+++ code/LinearModels.r
@@ -13,3 +13,42 @@ tip1
 0.92 + 0.10*15
 0.92 + 0.10*16
 0.92 + 0.10*0
+
+tip2 <- lm(tip ~ total_bill + size, data=tips)
+tip2
+
+0.668 + 0.092*30 + 0.193*1
+0.668 + 0.092*30 + 0.193*2
+0.668 + 0.092*45 + 0.193*2
+
+tip2
+summary(tip2)
+coefplot(tip2, sort='magnitude')
+
+head(tips)
+table(tips$day)
+
+source(
+    'https://raw.githubusercontent.com/jaredlander/LiveBeginnerFebruary2019/master/code/boros.r'
+)
+
+boros
+
+build.x( ~ Pop, data=boros)
+build.x( ~ Pop + Size, data=boros)
+build.x( ~ Pop * Size, data=boros)
+build.x( ~ Pop + Boro, data=boros)
+
+tip3 <- lm(tip ~ total_bill + size + day, data=tips)
+tip3
+summary(tip3)
+coefplot(tip3, sort='magnitude')
+
+tip4 <- lm(tip ~ total_bill + size + day + smoker, data=tips)
+coefplot(tip4, sort='magnitude')
+
+AIC(tip1, tip2, tip3, tip4)
+BIC(tip1, tip2, tip3, tip4)
+
+multiplot(tip1, tip2, tip3, tip4)
+multiplot(tip1, tip2, tip3, tip4, single=FALSE)
