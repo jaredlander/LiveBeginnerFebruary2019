c32ff98fc32c43de86d44f96ffaea5c12b831149
Jared Lander <info@jaredlander.com>
2019-02-05 12:24:29 -0500

    Added smoothing curves

--- code/Plotting.r
+++ code/Plotting.r
@@ -8,3 +8,25 @@ ggplot(diamonds, aes(x=carat, y=price))
 
 ggplot(diamonds, aes(x=carat, y=price)) + geom_point()
 ggplot(diamonds, aes(x=carat, y=price, color=cut)) + geom_point()
+ggplot(diamonds, aes(x=carat, y=price, color=cut)) + geom_point(shape=1)
+ggplot(diamonds, aes(x=carat, y=price, color=cut)) + geom_point(shape=1, size=1)
+
+ggplot(diamonds, aes(x=carat, y=price, color=cut, shape=cut)) + 
+    geom_point(size=1)
+
+ggplot(diamonds, aes(x=carat, y=price, color=cut)) + 
+    geom_point(shape=1, size=1) + 
+    geom_smooth()
+
+ggplot(diamonds, aes(x=carat, y=price)) + 
+    geom_point(shape=1, size=1) + 
+    geom_smooth()
+
+ggplot(diamonds, aes(x=carat, y=price)) + 
+    geom_point(shape=1, size=1, aes(color=cut)) +
+    geom_smooth()
+
+ggplot(diamonds, aes(x=carat, y=price)) + 
+    geom_point(shape=1, size=1, aes(color=cut)) +
+    geom_smooth() +
+    geom_smooth(aes(color=cut))
