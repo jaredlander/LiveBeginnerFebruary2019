4255c1529907418f9b01f2cbb461d94605082f00
Jared Lander <info@jaredlander.com>
2019-02-05 15:47:34 -0500

    Showed binary regression

---
+++ code/GLM.r
@@ -0,0 +1,19 @@
+data(tips, package='reshape2')
+library(coefplot)
+
+head(tips)
+table(tips$time)
+
+time1 <- glm(time ~ total_bill, data=tips, family=binomial)
+time1
+summary(time1)
+coefplot(time1)
+
+time2 <- glm(time ~ total_bill + day, data=tips, family=binomial)
+coefplot(time2, sort='magnitude')
+
+time3 <- glm(time ~ total_bill + smoker, data=tips, family=binomial)
+coefplot(time3, sort='magnitude')
+
+AIC(time1, time2, time3)
+BIC(time1, time2, time3)
