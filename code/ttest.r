a1de6e6a897d77ea1ff5a2901e379dfe310e4acf
Jared Lander <info@jaredlander.com>
2019-02-05 13:52:57 -0500

    Showed two-sided t-test

--- code/ttest.r
+++ code/ttest.r
@@ -4,3 +4,11 @@ head(tips)
 mean(tips$tip)
 
 t.test(tips$tip, mu=3.00, alternative='two.sided')
+hist(tips$tip)
+
+library(dplyr)
+tips %>% 
+    group_by(sex) %>% 
+    summarize(AvgTip=mean(tip))
+
+t.test(tip ~ sex, data=tips)
